91561a10eff3e2935940707a6fdee146bc1e968d
jlxu927 <xujingling0927@icloud.com>
2020-11-22 20:14:52 +0800

    Student pojo

---
+++ src/pojo/Student.java
@@ -0,0 +1,34 @@
+package pojo;
+
+public class Student {
+    private String stuId;
+    private String stuName;
+    private String stuClass;
+
+    public Student() {
+    }
+
+    public String getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(String stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getStuClass() {
+        return stuClass;
+    }
+
+    public void setStuClass(String stuClass) {
+        this.stuClass = stuClass;
+    }
+}
