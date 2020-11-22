ae89d71cea59f5895f9a5cae6e3d10060ba3fdf9
jlxu927 <xujingling0927@icloud.com>
2020-11-22 20:12:35 +0800

    Article pojo

---
+++ src/pojo/Article.java
@@ -0,0 +1,52 @@
+package pojo;
+
+public class Article {
+    private String artId;
+    private String artTitle;
+    private String artText;
+    private String artDate;
+    private String artWriter;
+
+    public Article() {
+    }
+
+    public String getArtId() {
+        return artId;
+    }
+
+    public void setArtId(String artId) {
+        this.artId = artId;
+    }
+
+    public String getArtTitle() {
+        return artTitle;
+    }
+
+    public void setArtTitle(String artTitle) {
+        this.artTitle = artTitle;
+    }
+
+    public String getArtText() {
+        return artText;
+    }
+
+    public void setArtText(String artText) {
+        this.artText = artText;
+    }
+
+    public String getArtDate() {
+        return artDate;
+    }
+
+    public void setArtDate(String artDate) {
+        this.artDate = artDate;
+    }
+
+    public String getArtWriter() {
+        return artWriter;
+    }
+
+    public void setArtWriter(String artWriter) {
+        this.artWriter = artWriter;
+    }
+}
