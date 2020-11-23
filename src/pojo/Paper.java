b0297cf67778351e82b082bf6031a2ec4323c2cc
jlxu927 <xujingling0927@icloud.com>
2020-11-23 21:59:51 +0800

    论文类

---
+++ src/pojo/Paper.java
@@ -0,0 +1,52 @@
+package pojo;
+
+public class Paper {
+    private String paperId;
+    private String paperTitle;
+    private String paperContext;
+    private String paperDate;
+    private String paperAuthor;
+
+    public Paper() {
+    }
+
+    public String getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(String paperId) {
+        this.paperId = paperId;
+    }
+
+    public String getPaperTitle() {
+        return paperTitle;
+    }
+
+    public void setPaperTitle(String paperTitle) {
+        this.paperTitle = paperTitle;
+    }
+
+    public String getPaperContext() {
+        return paperContext;
+    }
+
+    public void setPaperContext(String paperContext) {
+        this.paperContext = paperContext;
+    }
+
+    public String getPaperDate() {
+        return paperDate;
+    }
+
+    public void setPaperDate(String paperDate) {
+        this.paperDate = paperDate;
+    }
+
+    public String getPaperAuthor() {
+        return paperAuthor;
+    }
+
+    public void setPaperAuthor(String paperAuthor) {
+        this.paperAuthor = paperAuthor;
+    }
+}
